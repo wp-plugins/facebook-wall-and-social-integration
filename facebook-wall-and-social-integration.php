366c3ed3936b95299a9cb985436c954be2718396
mitsol <mitsol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-22 21:41:04 +0000

    v 1.2 changes
    git-svn-id: https://plugins.svn.wordpress.org/facebook-wall-and-social-integration/trunk@900492 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- facebook-wall-and-social-integration.php
+++ facebook-wall-and-social-integration.php
@@ -23,7 +23,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-//for free version -
+//for free version -  
 
 include_once dirname(__FILE__) . '/facebook-wall-and-social-integration-functions.php';
 include_once dirname(__FILE__) . '/admin/facebook-wall-admin.php';
